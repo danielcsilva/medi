9fb702409f40141d97d374fe65e8df080c42c196
Guilherme P. Lima <gui.pedrosa@gmail.com>
2020-02-16 11:50:19 -0300

    form for accessions process

--- resources/js/forms/accession/accession.js
+++ resources/js/forms/accession/accession.js
@@ -1,10 +1,18 @@
 window.$ = window.jQuery = require('jquery');
+require('jquery-mask-plugin');
 
-$(document).ready(function(){
+var dependents = 0;
+
+$(document).ready(function($){
+
+
+    $('.telephone').mask('(00) 0000-0000');
 
     $('#addTelephone').on('click', function(e){
         e.preventDefault();
         var item = $('.repeat-telephone:first').clone();
+        item.find('input').val('');
+        item.find('input').mask('(00) 0000-0000');
         item.find('.remove-telephone').show();
         $('#telephone-repeater').append(item);
     });
@@ -16,4 +24,52 @@ $(document).ready(function(){
         }
     });
 
+
+    $('#addColaborator').on('click', function(e){
+        e.preventDefault();
+        dependents += 1;
+
+        var items = $('#repeater-colaborator').clone();
+        
+        items.find('input').each(function(i,o){            
+            let placeholder = ($(o).attr('placeholder') ? $(o).attr('placeholder').replace('titular', 'dependente') : '');
+            $(o).attr('placeholder', placeholder);
+            $(o).attr('name', $(o).attr('name') + '_' + dependents);
+        });
+
+        var deleteBtn = '<a href="#" data-toggle="modal" data-target="#deleteModal" class="float-right"><i class="material-icons">delete_forever</i></a>';
+        var fieldset = '<fieldset class="form-group dependent"><span class="count">#' + dependents + '</span><span class="delete-dependent">' + deleteBtn  +'</span>' + items.html() + '</fieldset>';
+        
+
+        $('#dependents').append(fieldset);
+    })
+
+
+    $(document).on('click', '.delete-dependent', function(e){
+       $('#toDelete').val($(e.target).parents('fieldset:first').index());
+    });
+
+
+    $('#delete-dependent-confirm').on('click', function(e){
+        e.preventDefault();
+        
+        var toRemove = $('#toDelete').val();
+        $('fieldset:eq('+ toRemove +')').remove();
+
+        recountDependents();
+    })
+
 });
+
+
+function recountDependents() {    
+
+    var count = 0
+    $('fieldset.dependent').each(function(i,o){
+        console.log(i);
+        $(o).find('span.count').html('#' + (i + 1));
+        count++;
+    });
+
+    dependents = count;
+}   
--- resources/views/accessions/_form.blade.php
+++ resources/views/accessions/_form.blade.php
@@ -1,26 +1,39 @@
-<div class="form-row mb-4 mt-4 repeater-colaborator">
-    <div class="col-4">
-        <input type="text" name="beneficiary_name" class="form-control" placeholder="Nome do beneficiário titular"  @error('beneficiary_name') is-invalid @enderror" value="">
+<div id="repeater-colaborator">
+
+    <div class="form-row mb-4 mt-4">
+        <div class="col-6">
+            <input type="text" name="beneficiary_name" class="form-control" placeholder="Nome do beneficiário titular" value="">
+        </div>
+        <div class="col-6">
+            <input type="email" name="beneficiary_email" class="form-control" placeholder="Email">
+        </div>
     </div>
-    <div class="col-3">
-        <input type="text" name="beneficiary_email" class="form-control" placeholder="Email">
+    
+    <div class="form-row mb-4 mt-4">
+        <div class="col">
+            <input type="text" name="beneficiary_birth_date" class="form-control" placeholder="Data de Nasc.">
+        </div>
+        <div class="col">
+            <input type="text" name="beneficiary_height" class="form-control" placeholder="Altura">
+        </div>
+        <div class="col">
+            <input type="text" name="beneficiary_weight" class="form-control" placeholder="Peso">
+        </div>
+        <div class="col">        
+            <select class="custom-select mr-sm-2" id="inlineFormCustomSelect" name="beneficiary_gender">
+                <option value="">Sexo</option>
+                <option value="M">Masculino</option>
+                <option value="F">Feminino</option>
+            </select>
+        </div>
+        <div class="col">
+            <div class="form-check form-check-inline mt-2">
+                <input class="form-check-input" type="checkbox" id="financier" value="1">
+                <label class="form-check-label" for="financier">Financiador</label>
+            </div>
+        </div>
     </div>
-    <div class="col">
-        <input type="text" name="beneficiary_birth_date" class="form-control" placeholder="Data de Nasc.">
-    </div>
-    <div class="col">
-        <input type="text" name="beneficiary_height" class="form-control" placeholder="Altura">
-    </div>
-    <div class="col">
-        <input type="text" name="beneficiary_weight" class="form-control" placeholder="Peso">
-    </div>
-    <div class="col">        
-        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect" name="beneficiary_gender">
-            <option value="">Sexo</option>
-            <option value="M">Masculino</option>
-            <option value="F">Feminino</option>
-        </select>
-    </div>
+
 </div>
 
 
@@ -30,9 +43,7 @@
         <div class="form-group" id="telephone-repeater">            
             <label for="telephone">Telefones <a href="#" id="addTelephone">(+)</a></label>
             <div class="input-group repeat-telephone mb-2 mt-2">
-                <input type="text" class="form-control"
-                       name="beneficiary_telephone[]" aria-describedby="telephonelHelp"
-                       placeholder="(XX) xxxx-xxxx">
+                <input type="text" class="form-control telephone" name="beneficiary_telephone[]" aria-describedby="telephonelHelp">
                 <div class="input-group-append">
                     <span class="input-group-text remove-telephone" style="display:none;"><img src="/images/delete.png" alt=""></span>
                 </div>
@@ -47,6 +58,33 @@
 <div class="form-row mb-4 mt-4">
     <div class="col">
         <a href="#" id="addColaborator">Adicionar Dependente</a>
+    </div>
+</div>
+
+<div id="dependents">
+</div>
+
+
+<input type="hidden" id="toDelete" value="" />
+
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Excluir</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            Deseja realmente excluir o registro?
+           
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            <button type="button" class="btn btn-primary" data-dismiss="modal" id="delete-dependent-confirm">Sim</button>
+        </div>
+        </div>
     </div>
 </div>
 
