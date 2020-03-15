af3222527519664ed1ff3165727877430c5ffaa4
Guilherme Lima <gui.pedrosa@gmail.com>
2020-03-14 21:14:34 -0300

    adding user permissions, add new accession process status

---
+++ app/Http/Controllers/ProcessStatusController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ProcessStatus;
+use Illuminate\Http\Request;
+
+class ProcessStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProcessStatus  $processStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProcessStatus $processStatus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProcessStatus  $processStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProcessStatus $processStatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProcessStatus  $processStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProcessStatus $processStatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProcessStatus  $processStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProcessStatus $processStatus)
+    {
+        //
+    }
+}
